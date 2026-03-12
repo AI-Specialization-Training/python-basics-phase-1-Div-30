aeb595ce3abaf5caad679a7063ff0b5c2fee3105
Div-30 <divinpaul88@gmail.com>
2026-03-12 20:18:09 +0200

    feat: add  solution of question 5

--- exercise-5.py
+++ exercise-5.py
@@ -4,3 +4,7 @@
 #Create a function called repeat. 
 # It takes two parameters, a string and a count, and returns a new string that is the old one repeated count times.
 # ============================================================================
+
+def repeat(value, count):
+    return value * count
+print(repeat("amen", 5))
