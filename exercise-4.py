2d2b564f222d47aecdb479b082e99ddce1cc6daf
Div-30 <divinpaul88@gmail.com>
2026-03-12 20:09:46 +0200

    feat: add fourth solution

--- exercise-4.py
+++ exercise-4.py
@@ -10,3 +10,19 @@
 # return the string You can't convert {value} into a {type}., so again you use a formatted string.
 
 # ============================================================================
+
+# Solution
+def data_type_conversion(a, b):
+    try:
+        if b == "float":
+            return float(a)
+        elif b == "str":
+            return str(a)
+        elif b == "int":
+            return int(a)
+        else:
+            return "The data types must be float, str, or int"
+    except ValueError as error:
+        return f"Invalid data type {error}"
+print(data_type_conversion(1, "str"))
+
